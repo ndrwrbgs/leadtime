5def0b644a6ae786592ba4c8ee3049cfaca70461
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2020-01-23 16:19:34 +0100

    Add comment

--- LeadTime.Core/Core/LeadTimeMode.cs
+++ LeadTime.Core/Core/LeadTimeMode.cs
@@ -3,6 +3,10 @@
     public enum LeadTimeMode : ushort
     {
         ReportCommitAtShipDate,
+
+        /// <summary>
+        /// Note that only commits that HAVE SHIPPED will be reported, so newer data will be partial
+        /// </summary>
         ReportCommitAtCommitDate
     }
 }
